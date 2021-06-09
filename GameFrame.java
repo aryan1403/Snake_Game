f6b09676e1d43a1b91390faab4c58851ac6575b6
Hellboy-Aaryan <aaryan14032006@gmail.com>
2021-06-09 16:10:32 +0530

    Created GameFrame

---
+++ GameFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+    public GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Snake Game");
+        this.setDefaultCloseOperation(3);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.pack();
+        this.setVisible(true);
+    }
+}
