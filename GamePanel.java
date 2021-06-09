69d8efc88f4608bab025e4bed0a1eed7706a8508
Hellboy-Aaryan <aaryan14032006@gmail.com>
2021-06-09 16:10:56 +0530

    Created Panel class

---
+++ GamePanel.java
@@ -0,0 +1,13 @@
+import javax.swing.JPanel;
+import java.awt.event.*;
+
+public class GamePanel extends JPanel implements ActionListener {
+    public GamePanel() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
